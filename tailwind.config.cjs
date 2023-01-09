0c27dacd95520630aba35230f408d10d8a37f978
Oliver Davies <oliver@oliverdavies.uk>
2023-01-09 12:00:00 +0000

    Add navbar and jumbotron styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
+const colors = require('tailwindcss/colors');
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          750: "#343a40",
+          ...colors.neutral,
+        },
+      }
+    },
+  },
+  plugins: [
+    plugin(({ addVariant }) => {
+      addVariant('hocus', ['&:hover', '&:focus'])
+    }),
+  ],
 }
